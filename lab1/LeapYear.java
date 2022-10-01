423868efc4749983e6981f437bd497d59ffa6bd4
shalalalatuzki <18810320132@163.com>
2022-10-01 11:44:06 +0800

    finish lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,8 +3,11 @@
  */
 public class LeapYear {
 
-    private static bool isLeapYear(){
-        
+    private static boolean isLeapYear(int year){
+        if(year%400==0||year%4==0&&year%100!=0){
+            return true;
+        }
+        return false;
     }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
